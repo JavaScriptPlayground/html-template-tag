c4f0ba6971bbb83d23d4fd26453071a041a79a63
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-04-12 16:44:34 +0200

    Update template.js

--- template.js
+++ template.js
@@ -1,15 +1,15 @@
 const templates = []
 
 for (const template of document.getElementsByTagName('include')) {
-templates.push(template)
-await fetch(template.getAttribute('src'))
-.then(file => file.text())
-.then(content => {
-const html = document.createElement('html')
-html.innerHTML = content
-document.head.innerHTML += html.getElementsByTagName('head')[0].innerHTML
-template.insertAdjacentHTML('afterend', html.getElementsByTagName('body')[0].innerHTML)
-});
+  templates.push(template)
+  await fetch(template.getAttribute('src'))
+    .then(file => file.text())
+    .then(content => {
+  const html = document.createElement('html')
+  html.innerHTML = content
+  document.head.innerHTML += html.getElementsByTagName('head')[0].innerHTML
+  template.insertAdjacentHTML('afterend', html.getElementsByTagName('body')[0].innerHTML)
+  });
 }
 
 templates.forEach(template => template.remove())
